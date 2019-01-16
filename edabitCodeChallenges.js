7e1181455731bf45d222589e585b39f4e1096e1a
bt <btfranzen@gmail.com>
2019-01-15 18:33:56 -0700

    1/15 js challenges again

--- edabitCodeChallenges.js
+++ edabitCodeChallenges.js
@@ -561,7 +561,21 @@ Test.assertSimilar(noOdds([1, 2, 3, 4, 5, 6, 7, 8]), [2, 4, 6, 8]);
 Test.assertSimilar(noOdds([43, 65, 23, 89, 53, 9, 6]), [6]);
 Test.assertSimilar(noOdds([718, 991, 449, 644, 380, 440]), [718, 644, 380, 440]);
 
+/* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
+//  Create a function that takes an array of arrays with numbers. Return a new (single) array with the largest numbers of each.
+function findLargestNums(arr) {
+  	let retArr = []
 
+  	for(var i=0 ; i<arr.length ; i++) {
+    	retArr.push(Math.max(...arr[i]))
+    }
+  	return retArr
+}
+
+// tests
+Test.assertSimilar(findLargestNums([[4, 2, 7, 1], [20, 70, 40, 90], [1, 2, 0]]), [7, 90, 2])
+Test.assertSimilar(findLargestNums([[0.4321, 0.7634, 0.652], [1.324, 9.32, 2.5423, 6.4314], [9, 3, 6, 3]]), [0.7634, 9.32, 9])
+Test.assertSimilar(findLargestNums([[-34, -54, -74], [-32, -2, -65], [-54, 7, -43]]), [-34, -2, 7])
 
 
 
