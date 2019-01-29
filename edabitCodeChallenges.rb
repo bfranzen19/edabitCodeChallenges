7584efac56e2ab3ccf63b68255504e771beebfac
bt <btfranzen@gmail.com>
2019-01-28 20:15:03 -0700

    1/28 ruby challenges

--- edabitCodeChallenges.rb
+++ edabitCodeChallenges.rb
@@ -1,6 +1,6 @@
 ### profile link: https://edabit.com/user/zP84vhK6yKs3q7xtk
 ### -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ###
-### Create a function that takes a number as its only argument and returns true if it's less than or equal to zero, otherwise return false. 
+### Create a function that takes a number as its only argument and returns true if it's less than or equal to zero, otherwise return false.
 
 def less_than_or_equal_to_zero(num)
 	if num <= 0
@@ -15,7 +15,17 @@ Test.assert_equals(less_than_or_equal_to_zero(5), false)
 Test.assert_equals(less_than_or_equal_to_zero(0), true)
 Test.assert_equals(less_than_or_equal_to_zero(-5), true)
 
+### -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ###
+### Create a function that takes an array of numbers. Return the largest number in the array.
 
+def find_largest_num(nums)
+	return nums.max
+end
+
+### tests
+Test.assert_equals(find_largest_num([4, 5, 1, 3]), 5)
+Test.assert_equals(find_largest_num([13, 27, 18, 26]), 27)
+Test.assert_equals(find_largest_num([32, 35, 37, 39]), 39)
 
 
 
