6e63dd90f5b2a4838e6973c594c468310b78de01
bt <btfranzen@gmail.com>
2019-01-31 19:29:27 -0700

    1/31 java challenges

--- edabitCodeChallenges.java
+++ edabitCodeChallenges.java
@@ -685,11 +685,72 @@ public class ProgramTest {
 }
 
 /* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
+// Create a method that takes a Map<String, String> and return the values as a separate array.
+
+import java.util.Map;
+
+public class Program {
+    public static String[] getValues(Map<String,String> dict) {
+    	String[] ret = dict.values().toArray(new String[0]);
+	  	return ret;
+    }
+}
+
+// tests
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import java.util.Map;
+import java.util.HashMap;
+
+public class ProgramTest {
+
+    @Test
+    public void test1(){
+      	Map<String, String> map = new HashMap<String, String>();
+				map.put("a", "1");
+				map.put("b", "2");
+      	map.put("c", "3");
+        assertEquals(new String[]{"1","2","3"}, Program.getValues(map));
+    }
+
+    @Test
+    public void test2(){
+      	Map<String, String> map = new HashMap<String, String>();
+				map.put("a", "Apple");
+				map.put("b", "Microsoft");
+      	map.put("c", "Google");
+        assertEquals(new String[]{"Apple","Microsoft","Google"}, Program.getValues(map));
+    }
+
+  @Test
+    public void test3(){
+      	Map<String, String> map = new HashMap<String, String>();
+				map.put("key1", "true");
+				map.put("key2", "false");
+      	map.put("key3", "undefined");
+        assertEquals(new String[]{"true","false","undefined"}, Program.getValues(map));
+    }
+}
+
+/* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
 //
 
 
 
 // tests
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
