c815db6f8b833dd61c0e69899225d3b026695d9f
bfranzen19 <btfranzen@gmail.com>
2019-02-18 14:41:54 -0700

    2/18 php challenges

--- edabitCodeChallenges.php
+++ edabitCodeChallenges.php
@@ -48,13 +48,34 @@ class SumOf2NumbersTest extends TestCase {
   }
 }
 
+<!-- Create a function that takes a number as its only argument and returns true if it's less than or equal to zero, otherwise return false. -->
+
+function lessThanOrEqualToZero($num) {
+	if($num <= 0) {
+	  return true;
+	}
+  	else
+	  return false;
+}
+
+<!-- tests -->
+class IsNumberZeroOrNegativeTest extends TestCase {
+  public function testZeroOrNegative() {
+    $this->assertFalse(lessThanOrEqualToZero(5), "5 is more than zero");
+    $this->assertTrue(lessThanOrEqualToZero(0), "0 is equal zero");
+    $this->assertTrue(lessThanOrEqualToZero(-5), "-5 is less than zero");
+    $this->assertFalse(lessThanOrEqualToZero(1), "1 is more than zero");
+    $this->assertFalse(lessThanOrEqualToZero(2), "2 is more than zero");
+    $this->assertFalse(lessThanOrEqualToZero(10000), "10000 is more than zero");
+    $this->assertFalse(lessThanOrEqualToZero(0.5), "0.5 is more than zero");
+  }
+}
+
 <!--  -->
 
 
 
 <!-- tests -->
-
-
 
 
 
