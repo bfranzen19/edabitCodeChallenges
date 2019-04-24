f0eaff820ee0bae714db2e3b3ebc70531faaf424
bt <btfranzen@gmail.com>
2019-04-23 20:35:08 -0600

    4/23 python practice

--- edabitCodeChallenges.py
+++ edabitCodeChallenges.py
@@ -10,11 +10,50 @@ Test.assert_equals(tri_area(5, 4), 10)
 Test.assert_equals(tri_area(10, 10), 50)
 
 ### -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ###
+### Create a function that finds the maximum range of a triangles third edge. (side1 + side2) - 1 = maximum range of third edge.
+
+def nextEdge(side1, side2):
+	return (side1 + side2) - 1
+
+### tests
+Test.assert_equals(nextEdge(5, 4), 8)
+Test.assert_equals(nextEdge(8, 3), 10)
+Test.assert_equals(nextEdge(7, 9), 15)
+
+### -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ###
+### Create a function that returns True if a string is empty and False otherwise.
+
+def is_empty(s):
+	return len(s) == 0
+
+### tests
+Test.assert_equals(is_empty(""), True)
+Test.assert_equals(is_empty(" "), False)
+Test.assert_equals(is_empty("            "), False)
+
+### -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ###
+### Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+def addition(num):
+	return num + 1
+
+### tests
+Test.assert_equals(addition(2), 3)
+Test.assert_equals(addition(-9), -8)
+Test.assert_equals(addition(0), 1)
+
+### -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ###
 ###
 
 
 
 ### tests
+
+
+
+
+
+
 
 
 
