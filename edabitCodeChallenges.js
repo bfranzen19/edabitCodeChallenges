19b046481005a62b18dfea8cdb9878fb927d4521
bt franzen <btfranzen@gmail.com>
2019-06-17 10:16:14 -0600

    6/17 js practice

--- edabitCodeChallenges.js
+++ edabitCodeChallenges.js
@@ -2334,13 +2334,23 @@ Test.assertEquals(subReddit("https://www.reddit.com/r/mildlyinteresting/"), "mil
 Test.assertEquals(subReddit("https://www.reddit.com/r/funny/"), "funny")
 
 /* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
+// Create a function that finds the maximum range of a triangles third edge.
+
+function nextEdge(side1, side2) {
+	return (side1 + side2) - 1;
+}
+
+// tests
+Test.assertEquals(nextEdge(5, 4), 8)
+Test.assertEquals(nextEdge(8, 3), 10)
+Test.assertEquals(nextEdge(7, 9), 15)
+
+/* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
 //
 
 
 
 // tests
-
-
 
 
 
