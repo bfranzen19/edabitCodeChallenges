6204805c42bbbc9baca510de761c8109468cdd90
bt franzen <btfranzen@gmail.com>
2019-06-28 14:20:00 -0600

    6/28 python

--- edabitCodeChallenges.py
+++ edabitCodeChallenges.py
@@ -512,15 +512,39 @@ Test.assert_equals(same_case("HEllo"), False)
 Test.assert_equals(same_case("mArmALadE"), False)
 
 ### -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ###
+'''
+You are given 2 out of 3 of the angles in a triangle, in degrees.
+
+Write a function that classifies the missing angle as either "acute", "right", or "obtuse" based on its degrees.
+
+An acute angle is one smaller than 90 degrees.
+A right angle is one that is exactly 90 degrees.
+An obtuse angle is one greater than 90 degrees (but smaller than 180 degrees).
+For example: missing_angle(11, 20) should return "obtuse", since the missing angle would be 149 degrees, which makes it obtuse.
+'''
+
+def missing_angle(angle1, angle2):
+	total = 180
+	sum = angle1 + angle2
+
+	if total - sum < 90:
+		return 'acute'
+	elif total - sum == 90:
+		return 'right'
+	else:
+		return 'obtuse'
+
+### tests
+Test.assert_equals(missing_angle(27, 59), "obtuse")
+Test.assert_equals(missing_angle(135, 11), "acute")
+Test.assert_equals(missing_angle(45, 45), "right")
+
+### -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ###
 ###
 
 
 
 ### tests
-
-
-
-
 
 
 
