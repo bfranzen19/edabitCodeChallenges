97df8130a43b14e518c0ed438bf7ab7dfb32d938
bt franzen <btfranzen@gmail.com>
2019-06-28 14:02:04 -0600

    6/28 ruby

--- edabitCodeChallenges.rb
+++ edabitCodeChallenges.rb
@@ -671,11 +671,25 @@ Test.assert_equals(combo([1, 2, 3, 4], 3), [[1, 2, 3], [1, 2, 4], [1, 3, 4], [2,
 Test.assert_equals(combo([1, 2, 3, 4], 1), [[1], [2], [3], [4]])
 
 ### -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ###
+### Write a function that partitions the array into two subarrays: one with all even integers, and the other with all odd integers. Return your result in the following format:
+	# [[evens], [odds]]
+
+def even_odd_partition(arr)
+	return arr.partition { |e| e.even? }
+end
+
+### tests
+Test.assert_equals(even_odd_partition([5, 8, 9, 2, 0]), [[8, 2, 0], [5, 9]])
+Test.assert_equals(even_odd_partition([1, 0, 1, 0, 1, 0]), [[0, 0, 0], [1, 1, 1]])
+Test.assert_equals(even_odd_partition([8, 8, 4, 2]), [[8, 8, 4, 2], []])
+
+### -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ###
 ###
 
 
 
 ### tests
+
 
 
 
