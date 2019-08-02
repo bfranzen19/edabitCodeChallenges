23846bfb9c2cd175995958110b4e23f43cdb1d0f
bt <btfranzen@gmail.com>
2019-08-01 20:25:02 -0600

    8/1 js

--- edabitCodeChallenges.js
+++ edabitCodeChallenges.js
@@ -2673,14 +2673,31 @@ Test.assertEquals(limitNumber(-3, 1, 10), 1)
 Test.assertEquals(limitNumber(14, 1, 10), 10)
 
 /* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
+/*
+Suppose I want to define a function that removes the last element of an array each time I call it, but does not mutate the original array. Fix the code so that the results are no longer mutating the array.
+
+function minusOne(arr) {
+  arr.pop()
+  return arr
+}
+
+*/
+
+function minusOne(arr) {
+	return arr.slice(0, - 1);
+}
+
+// tests
+Test.assertSimilar(minusOne(x), [1, 2, 3, 4])
+Test.assertSimilar(minusOne(x), [1, 2, 3, 4])
+Test.assertSimilar(minusOne(x), [1, 2, 3, 4])
+
+/* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
 //
 
 
 
 // tests
-
-
-
 
 
 
