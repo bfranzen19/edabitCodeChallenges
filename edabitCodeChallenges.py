e32845298305d95ebbfd8a489bdb1b47f78ddefd
bt <btfranzen@gmail.com>
2019-08-15 08:54:07 -0600

    8/15 python

--- edabitCodeChallenges.py
+++ edabitCodeChallenges.py
@@ -650,11 +650,23 @@ Test.assert_equals(stack_boxes(2), 4)
 Test.assert_equals(stack_boxes(0), 0)
 
 ### -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ###
+### Create a function that takes a list and returns the first element.
+
+def get_first_value(number_list):
+	return number_list[0]
+
+### tests
+Test.assert_equals(get_first_value([1, 2, 3]), 1)
+Test.assert_equals(get_first_value([80, 5, 100]), 80)
+Test.assert_equals(get_first_value([-500, 0, 50]), -500)
+
+### -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ###
 ###
 
 
 
 ### tests
+
 
 
 
