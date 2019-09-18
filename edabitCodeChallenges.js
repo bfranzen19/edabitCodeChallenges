94773305a8ba0839e99fe9bfd8427668512a2761
bt <btfranzen@gmail.com>
2019-09-17 20:44:12 -0600

    9/17 js

--- edabitCodeChallenges.js
+++ edabitCodeChallenges.js
@@ -3651,12 +3651,28 @@ Test.assertEquals(factorial(3), 6)
 Test.assertEquals(factorial(1), 1)
 
 /* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
+/*
+Create a function that returns true if the first array can be nested inside the second.
+arr1 can be nested inside arr2 if:
+	arr1's min is greater than arr2's min.
+  	arr1's max is less than arr2's max.
+*/
+
+function canNest(arr1, arr2) {
+	return (Math.min(...arr1) > Math.min(...arr2) && Math.max(...arr1) < Math.max(...arr2)) ? true : false;
+}
+
+// tests
+Test.assertEquals(canNest([1, 2, 3, 4], [0, 6]), true)
+Test.assertEquals(canNest([3, 1], [4, 0]), true)
+Test.assertEquals(canNest([9, 9, 8], [8, 9, 10]), false)
+
+/* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
 //
 
 
 
 // tests
-
 
 
 
