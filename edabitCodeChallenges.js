15ac6cd818c336c2dff34370ac71d4fecaa6717f
bt <btfranzen@gmail.com>
2019-09-20 14:51:18 -0600

    9/20 js

--- edabitCodeChallenges.js
+++ edabitCodeChallenges.js
@@ -3708,6 +3708,34 @@ Test.assertEquals(binary(0), "0")
 /* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
 //
 
+function bitwiseAND(n1, n2) {
+	return n1 & n2;
+}
+
+function bitwiseOR(n1, n2) {
+	return n1 | n2;
+}
+
+function bitwiseXOR(n1, n2) {
+	return n1 ^ n2;
+}
+
+// tests
+Test.assertEquals(bitwiseAND(7, 12), 4)
+Test.assertEquals(bitwiseOR(7, 12), 15)
+Test.assertEquals(bitwiseXOR(7, 12), 11)
+
+Test.assertEquals(bitwiseAND(32, 17), 0)
+Test.assertEquals(bitwiseOR(32, 17), 49)
+Test.assertEquals(bitwiseXOR(32, 17), 49)
+
+Test.assertEquals(bitwiseAND(13, 19), 1)
+Test.assertEquals(bitwiseOR(13, 19), 31)
+Test.assertEquals(bitwiseXOR(13, 19), 30)
+
+/* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
+//
+
 
 
 // tests
