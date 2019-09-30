17d6db6a9138876cd206498ae9e82ecf20691930
bt <btfranzen@gmail.com>
2019-09-30 14:19:40 -0600

    9/30 js

--- edabitCodeChallenges.js
+++ edabitCodeChallenges.js
@@ -3806,11 +3806,54 @@ Test.assertEquals(indexShuffle("abababab"), "aaaabbbb")
 Test.assertEquals(indexShuffle("it was a beautiful day"), "i a  euiu atwsabatfldy")
 
 /* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
+// Create a function that moves all capital letters to the front of a word.
+
+function capToFront(s) {
+  return s.match(/[A-Z]/g).join('') + s.match(/[a-z]/g).join('');
+}
+
+// tests
+Test.assertEquals(capToFront("hApPy"), "APhpy")
+Test.assertEquals(capToFront("moveMENT"), "MENTmove")
+Test.assertEquals(capToFront("aPPlE"), "PPEal")
+
+/* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
+/*
+Write a function that returns the number of users in a chatroom based on the following rules:
+	If there is no one, return "no one online".
+	If there 1 person, return "[user1] online".
+	If there are 2 people, return [user 1] and [user 2] online".
+	If there are n>2 people, return the first two names and add "and n-2 more online".
+		For example, if there are 5 users, return: "[user1], [user2] and 3 more online"
+*/
+
+function chatroomStatus(users) {
+	switch (users.length) {
+	  	case 0:
+			return 'no one online';
+		case 1:
+			return `${users[0]} online`;
+			break;
+	  	case 2:
+			return `${users[0]} and ${users[1]} online`;
+			break;
+		default:
+			return `${users[0]}, ${users[1]} and ${users.length-2} more online`;
+	}
+}
+
+// tests
+Test.assertSimilar(chatroomStatus([]), "no one online")
+Test.assertSimilar(chatroomStatus(["becky325"]), "becky325 online")
+Test.assertSimilar(chatroomStatus(["becky325", "malcolm888"]), "becky325 and malcolm888 online")
+
+/* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
 //
 
 
 
 // tests
+
 
 
 
