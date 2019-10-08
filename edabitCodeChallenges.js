247066b03986314e4275b7d11de6d852dc8ba7b9
bt <btfranzen@gmail.com>
2019-10-07 18:05:32 -0600

    10/7 js

--- edabitCodeChallenges.js
+++ edabitCodeChallenges.js
@@ -3899,11 +3899,26 @@ Test.assertSimilar(spelling("cake"), ['c', 'ca', 'cak', 'cake'])
 Test.assertSimilar(spelling("happy"), ['h', 'ha', 'hap', 'happ', 'happy'])
 
 /* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
+// Write a function that receives two portions of a path and joins them. The portions will be joined with the "/" separator. There could be only one separator and if it is not present it should be added.
+
+function joinPath(portion1, portion2) {
+	return `${portion1.replace('/','')}/${portion2.replace('/','')}`;
+}
+
+// tests
+Test.assertEquals(joinPath("portion1", "portion2"), "portion1/portion2")
+Test.assertEquals(joinPath("portion1/", "portion2"), "portion1/portion2")
+Test.assertEquals(joinPath("portion1", "/portion2"), "portion1/portion2")
+
+/* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
 //
 
 
 
 // tests
+
+
+
 
 
 
