46d85bfbc34b5a25be96e684fb966027cd4debf0
bt <btfranzen@gmail.com>
2020-12-22 14:49:29 -0700

    Tuck in Array js

--- edabitCodeChallenges.js
+++ edabitCodeChallenges.js
@@ -4116,9 +4116,24 @@ Test.assertEquals(lessThan100(5, 57), true)
 Test.assertEquals(lessThan100(77, 30), false)
 Test.assertEquals(lessThan100(0, 59), true)
 
+/* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
+// Create a function that takes two arrays and insert the second array in the middle of the first array.
+
+function tuckIn(arr1, arr2) {
+  	return [arr1[0], ...arr2, arr1[1]];
+}
+
+// tests
+Test.assertSimilar(tuckIn([1, 10], [2, 3, 4, 5, 6, 7, 8, 9]), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+Test.assertSimilar(tuckIn([15, 150], [45, 75, 35]), [15, 45, 75, 35, 150])
+Test.assertSimilar(tuckIn(["bottom", "topping"], ["tomatosauce", "vegetables", "cheese"]), ["bottom", "tomatosauce", "vegetables", "cheese", "topping"])
+
+/* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
+//
 
 
 
+// tests
 
 
 
