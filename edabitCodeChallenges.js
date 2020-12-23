79bb4302478c05d7b321144a0626233a4acbfb9d
bt <btfranzen@gmail.com>
2020-12-23 12:33:48 -0700

    What's Hiding Amongst the Crowd js

--- edabitCodeChallenges.js
+++ edabitCodeChallenges.js
@@ -4165,12 +4165,25 @@ Test.assertEquals(f2(), "pear")
 Test.assertEquals(f3(), "")
 
 /* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
-//
+/*
+A word is on the loose and now has tried to hide amongst a crowd of tall letters! Help write a function to detect what the word is, knowing the following rules:
+	The wanted word is in lowercase.
+	The crowd of letters is all in uppercase.
+	Note that the word will be spread out amongst the random letters, but their letters remain in the same order.
+*/
 
+function detectWord(str) {
+	const found = str.match(/[a-z]/g);
+  	return found.join('');
+}
 
+// better solution
+const detectWord = str => str.replace(/[A-Z]/g, '');
 
 // tests
-
+Test.assertEquals(detectWord("UcUNFYGaFYFYGtNUH"), "cat")
+Test.assertEquals(detectWord("bEEFGBuFBRrHgUHlNFYaYr"), "burglar")
+Test.assertEquals(detectWord("YFemHUFBbezFBYzFBYLleGBYEFGBMENTment"), "embezzlement")
 
 
 
