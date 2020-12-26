a01929d42fdc054b7929d9f8d634723e98802456
bt <btfranzen@gmail.com>
2020-12-26 09:33:57 -0700

    Older Than Me js

--- edabitCodeChallenges.js
+++ edabitCodeChallenges.js
@@ -4215,11 +4215,52 @@ prom.then((res) => {
 Test.assertEquals(promise, promise)
 
 /* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
+/*
+Create a method in the Person class which returns how another person's age compares. Given the instances p1, p2 and p3, which will be initialised with the attributes name and age, return a sentence in the following format:
+
+{other person name} is {older than / younger than / the same age as} me.
+*/
+
+class Person {
+	constructor(name, age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	compareAge(other) {
+	  	let outcome;
+	  	if(this.age > other.age) { outcome = 'younger than';  }
+	  	else if(this.age < other.age) { outcome = 'older than'; }
+	  	else { outcome = 'the same age as';}
+
+	  	return `${other.name} is ${outcome} me.`;
+	}
+}
+
+// tests
+p1 = new Person("Samuel", 24)
+p2 = new Person("Joel", 36)
+p3 = new Person("Lily", 24)
+
+Test.assertEquals(p1.compareAge(p2), "Joel is older than me.")
+Test.assertEquals(p1.compareAge(p3), "Lily is the same age as me.")
+
+Test.assertEquals(p2.compareAge(p1), "Samuel is younger than me.")
+Test.assertEquals(p2.compareAge(p3), "Lily is younger than me.")
+
+Test.assertEquals(p3.compareAge(p1), "Samuel is the same age as me.")
+Test.assertEquals(p3.compareAge(p2), "Joel is older than me.")
+
+/* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
 //
 
 
 
 // tests
+
+
+
+
 
 
 
